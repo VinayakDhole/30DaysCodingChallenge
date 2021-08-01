1c090ba8b5369a039d371305a90ce6f276b41e2b
VZC\dholevi <vinayak.dhole@verizonconnect.com>
2021-08-01 13:47:31 +0100

    1st Day Challenge, Hello World!

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+
+            inputString = Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
